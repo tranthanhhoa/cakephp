5acfd56450265a2c2d2a526eed2fd8cc7ff06b78
Tran Thanh Hoa <phamhoa.coder@gmail.com>
2015-09-12 23:40:02 +0700

    Bookmarker Demo

---
+++ src/Controller/BookmarksController.php
@@ -0,0 +1,148 @@
+<?php
+namespace App\Controller;
+
+use App\Controller\AppController;
+use Cake\Utility\Text;
+
+/**
+ * Bookmarks Controller
+ *
+ * @property \App\Model\Table\BookmarksTable $Bookmarks
+ */
+class BookmarksController extends AppController
+{
+
+    public function isAuthorized($user)
+    {
+        $action = $this->request->params['action'];
+        if(in_array($action, ['index','add','tags'])){
+            return true;
+        }
+
+        // All other actions required an id
+        if(empty($this->request->params['pass'][0])){
+            return false;
+        }
+
+        // Check that the bookmark belong to user the current user
+        $id = $this->request->params['pass'][0];
+        $bookmark = $this->Bookmarks->get($id);
+        if($bookmark->user_id == $user['id']){
+            return true;
+        }
+        return parent::isAuthoried($user);
+    }
+
+    /**
+     * Index method
+     *
+     * @return void
+     */
+    public function index()
+    {
+        $this->paginate = [
+            'conditions' => ['Bookmarks.user_id' => $this->Auth->user('id')],
+            'contain' => ['Users']
+        ];
+        $this->set('bookmarks', $this->paginate($this->Bookmarks));
+        $this->set('_serialize', ['bookmarks']);
+    }
+
+    /**
+     * View method
+     *
+     * @param string|null $id Bookmark id.
+     * @return void
+     * @throws \Cake\Network\Exception\NotFoundException When record not found.
+     */
+    public function view($id = null)
+    {
+        $bookmark = $this->Bookmarks->get($id, [
+            'contain' => ['Users', 'Tags']
+        ]);
+        $this->set('bookmark', $bookmark);
+        $this->set('_serialize', ['bookmark']);
+    }
+
+    /**
+     * Add method
+     *
+     * @return void Redirects on successful add, renders view otherwise.
+     */
+    public function add()
+    {
+        $bookmark = $this->Bookmarks->newEntity();
+        if ($this->request->is('post')) {
+            $bookmark = $this->Bookmarks->patchEntity($bookmark, $this->request->data);
+            $bookmark->user_id = $this->Auth->user('id');
+            if ($this->Bookmarks->save($bookmark)) {
+                $this->Flash->success(__('The bookmark has been saved.'));
+                return $this->redirect(['action' => 'index']);
+            } else {
+                $this->Flash->error(__('The bookmark could not be saved. Please, try again.'));
+            }
+        }
+        $users = $this->Bookmarks->Users->find('list', ['limit' => 200]);
+        $tags = $this->Bookmarks->Tags->find('list', ['limit' => 200]);
+        $this->set(compact('bookmark', 'users', 'tags'));
+        $this->set('_serialize', ['bookmark']);
+    }
+
+    /**
+     * Edit method
+     *
+     * @param string|null $id Bookmark id.
+     * @return void Redirects on successful edit, renders view otherwise.
+     * @throws \Cake\Network\Exception\NotFoundException When record not found.
+     */
+    public function edit($id = null)
+    {
+        $bookmark = $this->Bookmarks->get($id, [
+            'contain' => ['Tags']
+        ]);
+        if ($this->request->is(['patch', 'post', 'put'])) {
+            $bookmark = $this->Bookmarks->patchEntity($bookmark, $this->request->data);
+            $bookmark->user_id = $this->Auth->user('id');
+            if ($this->Bookmarks->save($bookmark)) {
+                $this->Flash->success(__('The bookmark has been saved.'));
+                return $this->redirect(['action' => 'index']);
+            } else {
+                $this->Flash->error(__('The bookmark could not be saved. Please, try again.'));
+            }
+        }
+        $users = $this->Bookmarks->Users->find('list', ['limit' => 200]);
+        $tags = $this->Bookmarks->Tags->find('list', ['limit' => 200]);
+        $this->set(compact('bookmark', 'users', 'tags'));
+        $this->set('_serialize', ['bookmark']);
+    }
+
+    /**
+     * Delete method
+     *
+     * @param string|null $id Bookmark id.
+     * @return void Redirects to index.
+     * @throws \Cake\Network\Exception\NotFoundException When record not found.
+     */
+    public function delete($id = null)
+    {
+        $this->request->allowMethod(['post', 'delete']);
+        $bookmark = $this->Bookmarks->get($id);
+        if ($this->Bookmarks->delete($bookmark)) {
+            $this->Flash->success(__('The bookmark has been deleted.'));
+        } else {
+            $this->Flash->error(__('The bookmark could not be deleted. Please, try again.'));
+        }
+        return $this->redirect(['action' => 'index']);
+    }
+
+    public function tags()
+    {
+        $tags = $this->request->params['pass'];
+
+        $bookmarks = $this->Bookmarks->find('tagged', [
+            'tags' => $tags
+        ]);
+
+        $this->set(['bookmarks' => $bookmarks, 'tags' => $tags]);
+    }
+}
